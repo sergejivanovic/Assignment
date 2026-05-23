8e8d79d07edb5d30cfe93f9d2412d550f3204521
Sergej <sergej357.i@gmail.com>
2026-05-23 12:02:26 +0200

    Created class Item.

---
+++ Assignment/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Media;
+
+namespace Assignment.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Priority { get; set; }
+
+        public Brush Color
+        {
+            get
+            {
+                if (Priority == 1)
+                {
+                    return Brushes.Red;
+                }
+
+                if (Priority == 2)
+                {
+                    return Brushes.Yellow;
+                }
+
+                return Brushes.Green;
+            }
+        }
+    }
+}
